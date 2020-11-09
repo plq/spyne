1bcd6e172ecd83e046499006a955c074b51c1625
Burak Arslan <burak.arslan@arskom.com.tr>
2020-11-09 09:47:13 +0300

    #648 switch to debian-sid packages for testing
    debian sid test using spyne test scripts worked, switched to
    debian packages. this works as well save for missing packages.

--- requirements/debian_sid.sh
+++ requirements/debian_sid.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
-N=spyne-test-debian-sid
-sudo lxc launch images:debian/sid $N
-sudo lxc start $N
-sudo lxc exec $N -- apt-get update
-sudo lxc exec $N -- apt-get upgrade
-sudo lxc exec $N -- apt-get install -y git python3-spyne tox
-sudo lxc exec $N -- git clone https://github.com/arskom/spyne
-sudo lxc exec $N -- bash -c "cd spyne; virtualenv -p python3 virt-3; source virt-3/bin/activate; pip install -r requirements/test_requirements.txt; python setup.py test"
+export N=spyne-test-debian-sid
+{ sudo lxc launch images:debian/sid $N || sudo lxc start $N; } && exit 1
+
+until sudo lxc exec $N -- apt-get update; do sleep 1; done
+
+set -e
+
+sudo lxc exec $N -- apt-get upgrade || exit 20
+sudo lxc exec $N -- apt-get install -y git tox python3-{spyne,twisted,coverage,pytest-{cov,twisted,django},lxml,colorama,django,pyparsing,sqlalchemy,yaml,msgpack,pyramid,webtest,zeep,zmq,werkzeug}
+sudo lxc exec $N -- bash -c 'if [ ! -d spyne ]; then git clone https://github.com/arskom/spyne; fi'
+sudo lxc exec $N -- bash -c 'cd spyne && git pull'
+#sudo lxc exec $N -- bash -c 'cd spyne && python3 -m pytest --ignore=spyne/test/interop --ignore=examples'
+sudo lxc exec $N -- bash -c 'cd spyne && python3 setup.py test'
